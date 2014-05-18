82b9678c159ef53c6cd3f445407dccd002f2ded9
Argaman Aloni Mordoch <argaman.aloni@gmail.com>
2014-05-18 20:24:32 +0300

    added the new empty list view

--- src/com/technion/studybuddy/Views/LinksViewActivity.java
+++ src/com/technion/studybuddy/Views/LinksViewActivity.java
@@ -1,7 +1,11 @@
 package com.technion.studybuddy.Views;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -11,7 +15,9 @@ public class LinksViewActivity extends StudyBuddyActivity {
 
 	TextView courseNameTv;
 	TextView courseIdTv;
+	TextView emptyTv;
 	ListView linksLv;
+	List<String> links;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +25,20 @@ public class LinksViewActivity extends StudyBuddyActivity {
 		setContentView(R.layout.links_view_layout);
 		courseNameTv = (TextView) findViewById(R.id.textView1);
 		courseIdTv = (TextView) findViewById(R.id.textView2);
+		linksLv = (ListView) findViewById(R.id.listView1);
+		emptyTv = (TextView) findViewById(R.id.links_empty_tv);
 		Intent intent = getIntent();
 		courseNameTv.setText(intent.getStringExtra("CourseName"));
 		courseIdTv.setText(intent.getStringExtra("courseId"));
-		linksLv = (ListView) findViewById(R.id.listView1);
+		links = splitStringToList(intent.getStringExtra("LinksList"));
+		if (links.isEmpty())
+			emptyTv.setVisibility(View.VISIBLE);
 		// linksLv.setAdapter(new SimpleAdapter(getApplicationContext(), data,
 		// android.R.layout.simple_list_item_1, from, to));
+	}
+
+	private List<String> splitStringToList(String stringExtra) {
+		// TODO Auto-generated method stub
+		return new ArrayList<String>();
 	}
 }
