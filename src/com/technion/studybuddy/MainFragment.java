90f9a0eaddbbf6cf822356e1025a17d5f3cb147f
Argaman Aloni Mordoch <argaman.aloni@gmail.com>
2014-06-04 11:00:13 +0300

    added the new notification bubble. now adding it's animation and were done with this :D

--- src/com/technion/studybuddy/MainFragment.java
+++ src/com/technion/studybuddy/MainFragment.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.technion.studybuddy;
 
@@ -11,9 +11,13 @@ import org.achartengine.GraphicalView;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -40,13 +44,13 @@ import com.technion.studybuddy.charts.PieChartBuilder;
 import com.technion.studybuddy.data.DataStore;
 import com.technion.studybuddy.graphs.GraphFactory;
 import com.technion.studybuddy.models.Courses;
+import com.technion.studybuddy.utils.Constants;
 
 /**
  * @author Argaman
- * 
+ *
  */
-public class MainFragment extends Fragment implements Observer
-{
+public class MainFragment extends Fragment implements Observer {
 
 	public static final int USER_PERMISSION1 = 0;
 
@@ -58,9 +62,11 @@ public class MainFragment extends Fragment implements Observer
 	private NowLayout layout;
 	private View rootView;
 	private View semesterData;
+	private int locationInArray;
 
-	private void initInitialView()
-	{
+	private SharedPreferences sharedPref;
+
+	private void initInitialView() {
 		layout = (NowLayout) rootView.findViewById(R.id.course_list);
 		emptyState = (LinearLayout) rootView
 				.findViewById(R.id.stb_main_empty_state);
@@ -68,12 +74,10 @@ public class MainFragment extends Fragment implements Observer
 		layout.setAdapter(adapter);
 		Button btn = (Button) rootView
 				.findViewById(R.id.stb_main_empty_state_button);
-		btn.setOnClickListener(new OnClickListener()
-		{
+		btn.setOnClickListener(new OnClickListener() {
 
 			@Override
-			public void onClick(View v)
-			{
+			public void onClick(View v) {
 				startActivityForResult(new Intent(v.getContext(),
 						EditCourse.class), Activity.RESULT_CANCELED);
 			}
@@ -85,8 +89,7 @@ public class MainFragment extends Fragment implements Observer
 		chartLayout.setClickable(true);
 	}
 
-	private void initSemester()
-	{
+	private void initSemester() {
 		semesterData = LayoutInflater.from(rootView.getContext()).inflate(
 				R.layout.stb_view_simester_data, null);
 		TextView simterTextView = (TextView) semesterData
@@ -99,18 +102,16 @@ public class MainFragment extends Fragment implements Observer
 		weekCount.setText("week "
 				+ String.valueOf(currentWeek < DataStore.semester
 						.getTotalWeeks() ? currentWeek : DataStore.semester
-						.getTotalWeeks()
-						+ " / "
-						+ DataStore.semester.getTotalWeeks()));
+								.getTotalWeeks()
+								+ " / "
+								+ DataStore.semester.getTotalWeeks()));
 		updateGraphView();
 	}
 
 	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data)
-	{
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		DataStore.getInstance().notifyObservers();
-		switch (requestCode)
-		{
+		switch (requestCode) {
 		// if the user approved the use of the account make another request
 		// for the auth token else display a message
 		case GoogleHttpContext.USER_PERMISSION:
@@ -122,35 +123,55 @@ public class MainFragment extends Fragment implements Observer
 	}
 
 	@Override
-	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater)
-	{
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
 		inflater.inflate(R.menu.stb_main_menu, menu);
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState)
-	{
+			Bundle savedInstanceState) {
 		rootView = inflater.inflate(R.layout.stb_view_main, container, false);
 
 		DataStore.setContext(rootView.getContext());
 
+		Context context = getActivity();
+		sharedPref = context.getSharedPreferences(
+				Constants.popupSaredPrefrence, Context.MODE_PRIVATE);
+		if (savedInstanceState == null)
+			locationInArray = sharedPref.getInt(Constants.locationInArray, 0);
+		else
+			locationInArray = savedInstanceState
+			.getInt(Constants.locationInArray);
 		initInitialView();
 		setVisibilityEmptyState();
 		DataStore.getInstance().addObserver(this);
 		initSemester();
+
+		if ((getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) == Configuration.SCREENLAYOUT_SIZE_LARGE) {
+			// on a large screen device ...
+			TextView bubbleTv = (TextView) rootView
+					.findViewById(R.id.bubble_main_tv);
+			Log.d("BIG SCREEN", "got to the big screen resolution");
+			String[] array = getResources().getStringArray(
+					R.array.popup_notifications);
+			bubbleTv.setText(array[locationInArray % array.length]);
+		}
 
 		setHasOptionsMenu(true);
 		return rootView;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putInt(Constants.popupSaredPrefrence, locationInArray);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
 
 		Intent intent = null;
-		switch (item.getItemId())
-		{
+		switch (item.getItemId()) {
 		case android.R.id.home:
 
 			NavUtils.navigateUpFromSameTask(getActivity());
@@ -182,27 +203,22 @@ public class MainFragment extends Fragment implements Observer
 		return super.onOptionsItemSelected(item);
 	}
 
-	public void setDrawerAdapter(DrawerAdapter adapter2)
-	{
+	public void setDrawerAdapter(DrawerAdapter adapter2) {
 		drawerAdapter = adapter2;
 	}
 
-	private void setVisibilityEmptyState()
-	{
-		if (adapter.getCount() == 0)
-		{
+	private void setVisibilityEmptyState() {
+		if (adapter.getCount() == 0) {
 			emptyState.setVisibility(View.VISIBLE);
 			layout.setVisibility(View.GONE);
-		} else
-		{
+		} else {
 			layout.setVisibility(View.VISIBLE);
 			emptyState.setVisibility(View.GONE);
 		}
 	}
 
 	@Override
-	public void update(Observable observable, Object data)
-	{
+	public void update(Observable observable, Object data) {
 		updateGraphView();
 		adapter.notifyDataSetChanged();
 		if (drawerAdapter != null)
@@ -210,21 +226,17 @@ public class MainFragment extends Fragment implements Observer
 		setVisibilityEmptyState();
 	}
 
-	private void updateGraphView()
-	{
+	private void updateGraphView() {
 		chartLayout.removeAllViews();
 		Date today = new Date();
 		graphView = GraphFactory.getWeeklyProgressGraph(rootView.getContext(),
 				today, DataStore.getInstance().getWorkStats(today, 7));
 		graphView.setClickable(true);
-		graphView.setOnTouchListener(new OnTouchListener()
-		{
+		graphView.setOnTouchListener(new OnTouchListener() {
 
 			@Override
-			public boolean onTouch(View v, MotionEvent event)
-			{
-				switch (event.getAction())
-				{
+			public boolean onTouch(View v, MotionEvent event) {
+				switch (event.getAction()) {
 				case MotionEvent.ACTION_DOWN:
 					v.setAlpha((float) 0.5);
 					return false;
@@ -240,12 +252,10 @@ public class MainFragment extends Fragment implements Observer
 
 		});
 
-		graphView.setOnClickListener(new OnClickListener()
-		{
+		graphView.setOnClickListener(new OnClickListener() {
 
 			@Override
-			public void onClick(View v)
-			{
+			public void onClick(View v) {
 				Intent intent = new Intent(v.getContext(),
 						PieChartBuilder.class);
 				startActivity(intent);
@@ -259,27 +269,42 @@ public class MainFragment extends Fragment implements Observer
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see android.app.Fragment#onResume()
 	 */
 	@Override
-	public void onResume()
-	{
+	public void onResume() {
 		super.onResume();
 		DataStore.getInstance().addObserver(this);
 	}
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see android.app.Fragment#onPause()
 	 */
 	@Override
-	public void onPause()
-	{
-		// TODO Auto-generated method stub
+	public void onPause() {
 		super.onPause();
 		DataStore.getInstance().deleteObserver(this);
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		Bundle bundle = new Bundle();
+		if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
+			onSaveInstanceState(bundle);
+		else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT)
+			onSaveInstanceState(bundle);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		SharedPreferences.Editor editor = sharedPref.edit();
+		editor.putInt(Constants.locationInArray, ++locationInArray);
+		editor.commit();
 	}
 
 }
